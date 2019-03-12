15563628fb6146c68e145cea8b9a3dd9de15705a
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-12 15:25:45 +0530

    Added home and about pages

--- api.py
+++ api.py
@@ -2,10 +2,25 @@ from webob import Request, Response
 
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
     def __call__(self, environ, start_response):
         request = Request(environ)
-
-        response = Response()
-        response.text = "Hello, World!"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        response = Response()
+
+        for path, handler in self.routes.items():
+            if path == request.path:
+                handler(request, response)
+                return response
