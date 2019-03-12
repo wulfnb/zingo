a30ab7b920ed8b986fb2c999bcc5fadba57c08a8
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-12 16:17:05 +0530

    Pass values via url

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from webob import Request, Response
+from parse import parse
 
 
 class API:
@@ -19,10 +20,10 @@ class API:
 
     def handle_request(self, request):
         response = Response()
-        handler = self.find_handler(request_path=request.path)
+        handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
-            handler(request, response)
+            handler(request, response, **kwargs)
         else:
             self.default_response(response)
 
@@ -30,8 +31,11 @@ class API:
 
     def find_handler(self, request_path):
         for path, handler in self.routes.items():
-            if path == request_path:
-                return handler
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+
+        return None, None
 
     def default_response(self, response):
         response.status_code = 404
--- app.py
+++ app.py
@@ -11,3 +11,8 @@ def home(request, response):
 @app.route("/about")
 def about(request, response):
     response.text = "Hello from the ABOUT page"
+
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
