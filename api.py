94ab877794598ea94aa65ba65e60b380c0e00676
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-12 15:43:38 +0530

    Added find route

--- api.py
+++ api.py
@@ -19,13 +19,19 @@ class API:
 
     def handle_request(self, request):
         response = Response()
+        handler = self.find_handler(request_path=request.path)
 
-        for path, handler in self.routes.items():
-            if path == request.path:
-                handler(request, response)
-                return response
-        self.default_response(response)
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
+
         return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
 
     def default_response(self, response):
         response.status_code = 404
